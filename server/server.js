fdf3d04301aed82cdff68b432d8bc05dba6367e4
Camille Williams <camiwills325@gmail.com>
2023-03-24 19:45:13 -0400

    Delete evidence of students in code

--- server/server.js
+++ server/server.js
@@ -110,29 +110,6 @@ app.delete("/api/individual/:id", async (req, res) => {
   }
 })
 
-//A put request - Update a student 
-// app.put('/api/students/:studentId', cors(), async (req, res) =>{
-//   console.log(req.params);
-//   //This will be the id that I want to find in the DB - the student to be updated
-//   const studentId = req.params.studentId
-//   const updatedStudent = { id: req.body.id, firstname: req.body.firstname, lastname: req.body.lastname}
-//   console.log("In the server from the url - the student id", studentId);
-//   console.log("In the server, from the react - the student to be edited", updatedStudent);
-//   // UPDATE students SET lastname = "something" WHERE id="16";
-//   const query = `UPDATE students SET lastname=$1, firstname=$2 WHERE id=${studentId} RETURNING *`;
-//   const values = [updatedStudent.lastname, updatedStudent.firstname];
-//   try {
-//     const updated = await db.query(query, values);
-//     console.log(updated.rows[0]);
-//     res.send(updated.rows[0]);
-
-//   }catch(e){
-//     console.log(e);
-//     return res.status(400).json({e})
-//   }
-// })
-
-
 
 // console.log that your server is up and running
 app.listen(PORT, () => {
